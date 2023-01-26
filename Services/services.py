0b7f4d4fdb7b77494125023aa5bf236c3586a089
Garrett (GWWM) <114422880+GWWMcg@users.noreply.github.com>
2023-01-26 16:53:21 +0000

    Add files via upload

---
+++ Services/services.py
@@ -0,0 +1,22 @@
+from Models.models import Address, Account, Customer
+from Repository.repository import Repository
+
+class Services:
+
+    def __init__(self, repository = Repository()):
+        self.repository = repository
+
+    def open_account(self, account: Account) -> Account:
+
+        address = self.repository.insert_address(account.customer.address)
+
+        account.customer.address = address
+
+        customer = self.repository.insert_customer(account.customer)
+
+        account.customer = customer
+
+        return self.repository.insert_account(account)
+
+    def get_accounts(self):
+        return self.repository.get_accounts()
