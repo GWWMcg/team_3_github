6e830ab77441a4f12492d80b96872b923cdca880
Training <training@ip-172-31-28-44.ec2.internal>
2023-01-27 17:06:07 +0000

    :)

--- Controllers/main.py
+++ Controllers/main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
-from Services.services import Services
-from Models.models import Account
+from Services.services import AddressService, Services, AccountService, AddressRepository
+from Models.models import Account, Address, Customer
 app = FastAPI()
 
 service = Services()
 
-@app.get("/account")
+@app.get("/list_account")
 async def get_accounts():
-    return service.get_accounts()
+    return AccountService.get_accounts()
 
-@app.post("/account")
+@app.get("/accounts")
+async def get_accounts(id: str):
+    return AccountService.get_account(id=id)
+
+@app.post("/open_account")
 async def open_account(account: Account):
-    return service.open_account(account)
+    return AccountService.open_account(account)
+
+app.get('/list_addresses')
+async def get_addresses():
+    return AddressService.get_addresses()
+
+app.get('/address')
+async def get_addresses(id):
+    return AddressService.get_address(id=id)
+
+app.post('/open_address')
+async def open_address(address: Address):
+    return AddressService.insert_address(address)
+
+# app.get("/")
