07f52610243fb9254d6513ee8766ecf0a5a90369
GWWMcg <mooregarrettcg@capgroup.com>
2023-01-27 17:54:09 +0000

    Fix objects

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ from Services.services import AddressService, AccountService, AddressRepository
 from Models.models import Account, Address, Customer
 app = FastAPI()
 
+acc_ser = AccountService()
+add_ser = AddressService()
+
 @app.get("/list_account")
 async def get_accounts():
-    return AccountService.get_accounts()
+    return acc_ser.get_accounts()
 
 @app.get("/accounts")
 async def get_accounts(id: str):
-    return AccountService.get_account(id=id)
+    return acc_ser.get_account(id=id)
 
 @app.post("/open_account")
 async def open_account(account: Account):
-    return AccountService.open_account(account)
+    return acc_ser.open_account(account)
 
 @app.get('/list_addresses')
 async def get_addresses():
-    return AddressService.get_addresses()
+    return add_ser.get_addresses()
 
 @app.get('/address')
 async def get_addresses(id):
-    return AddressService.get_address(id=id)
+    return add_ser.get_address(id=id)
 
 @app.post('/open_address')
 async def open_address(address: Address):
-    return AddressService.insert_address(address)
+    return add_ser.insert_address(address)
 
 # app.get("/")
